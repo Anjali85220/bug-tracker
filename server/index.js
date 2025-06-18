68542d0e34e61f1f8472ae7806440b38b4ed8fa1
Anjali85220 <shivanjalidumpala8@gmail.com>
2025-06-18 11:18:49 +0530

    Final Commit backend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ Allow only your frontend (Vercel) URL
 app.use(cors({
-  origin: 'https://bug-tracker-ldle0ots7-shivanjali-dumpalas-projects.vercel.app', // ⬅️ Replace with your actual Vercel frontend URL
+  origin: 'https://bug-tracker-r1ldsrboq-shivanjali-dumpalas-projects.vercel.app', // ⬅️ Replace with your actual Vercel frontend URL
   credentials: true
 }));
 
